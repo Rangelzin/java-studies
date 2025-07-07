5dccd08c0a36bc112a0e68c2a527f564f67cd06e
Rangelzin <victorrangeltasse85@hotmail.com>
2025-07-07 16:42:03 -0300

    feat: Inicio do modulo iniciante, dados primitivos

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,16 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+         /*
+         * Dados Primitivos - int, double, float, char, boolean, short.
+         * Objetivo da aula: Criar um ninja - Rangel Uchiha
+         */
+
+        int idade = 19; // Valor máximo = 2 147 483 647
+        double altura = 1.69;
+        char inicial = 'N';
+        boolean VivoOuMOrto = true;
+        Long saldoBancario = 99999L;
+    }
+}
