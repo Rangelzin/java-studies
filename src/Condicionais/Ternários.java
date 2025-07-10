93865877bee32186877d691a9bc0dbcaa6167ba3
Rangelzin <victorrangeltasse85@hotmail.com>
2025-07-10 15:32:18 -0300

    feat: ternários em java

---
+++ src/Condicionais/Ternários.java
@@ -0,0 +1,14 @@
+package Condicionais;
+
+public class Ternários {
+    public static void main(String[] args) {
+         /* *
+         *  Ternários: São maneiras de reduzir o codigo
+         *  Variavel = (codinção) ? seforverdadeiro : seforfalso;
+         * */
+
+        short numeroMissoes = 11;
+        String nivelNinja = (numeroMissoes >= 10) ? "Esse ninja esta com mais de 10 missões" : "Esse ninja tem menos que 10 missões";
+        System.out.println(nivelNinja);
+    }
+}
